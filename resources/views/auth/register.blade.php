017638d094092ddbe715fc0f1641b153204b5685
hodamagdi <hodamagdi88@gmail.com>
2024-02-22 16:39:10 +0200

    reg blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,11 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
+            <div class="mt-4">
+                <x-label for="user_handle" value="{{ __('User handle') }}" />
+                <x-input id="user_handle" class="block mt-1 w-full" type="text" name="user_handle" required autocomplete="userhandel" />
+            </div>
+
             <div>
                 <x-label for="name" value="{{ __('Name') }}" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
@@ -28,6 +33,12 @@
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
+
+            <div class="mt-4">
+                <x-label for="gender" value="{{ __('gender') }}" />
+                <input id="gender-male" type="radio" name="gender" value="male"> Male
+                <input id="gender-female"style="margin-left: 20px;" type="radio" name="gender" value="female"> Female
+            </div>            
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
