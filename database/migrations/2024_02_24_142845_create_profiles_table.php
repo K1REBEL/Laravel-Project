c6730dc9b94dca8975340f596d48fb72c300505d
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-02-24 16:35:34 +0200

    add profile migration & model

--- database/migrations/2024_02_24_142845_create_profiles_table.php
+++ database/migrations/2024_02_24_142845_create_profiles_table.php
@@ -11,15 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('FullName',100);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
+
+            $table->string('Phone_No');
+
+            $table->string('Website');
+
+            $table->string('Bio');
+
+            $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->string('gender');
+            
+            $table->unsignedBigInteger('user_id');
+
             $table->timestamps();
-            $table->string('user_handle');
         });
     }
 
@@ -28,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
