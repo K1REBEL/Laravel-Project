65ed196e17f2930fac707259c50d05ec378f2ec0
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-02-27 17:49:11 +0200

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
@@ -18,6 +21,55 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/welcome', function () {
+    return view('welcomepage.instawelcome');
+});
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->resource('users', UserController::class);
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::redirect('/dashboard', '/users');
+});
+
+
+
+
+Route::get('/users/home', function(){
+    return view('users.home');
+});
+
+
+// Route::get('/users/follower', function(){
+//     return view('users.follower');
+// });
+
+
+Route::resource('/posts', PostController::class);
+Route::resource('/users', UserController::class);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
@@ -31,26 +83,26 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/profile', function () {
-    return view('userProfile.myprofile');
-});
 
-Route::get('/edit', function () {
-    return view('userProfile.edit');
-});
-
-Route::get('/profile/user', function () {
-    return view('userProfile.otherprofile');
-});
 
 Route::get('/users/home', function(){
     return view('users.home');
 });
 
-Route::get('/users/follower', function(){
-    return view('users.follower');
+
+Route::get('/users/followers', function(){
+    return view('users.followers');
 });
 
-Route::get('/post/create', function(){
-    return view('userprofile.createpost');
+
+
+Route::get('/users/following', function(){
+    return view('users.following');
 });
+
+Route::get('/users/blocked', function(){
+    return view('users.blocked');
+});
+
+// hello
+
