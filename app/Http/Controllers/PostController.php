b32399d546918651ee0f93d166225e2570cdc265
K1REBEL <kibraheem47@yahoo.com>
2024-02-29 10:24:22 +0200

    Minor Merge

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
-//use App\Models\Media;
+use App\Models\Media;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
