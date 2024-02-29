f730f472a4b7125153c2a53fd2eadf449b01c600
ALAA-20001 <151511514+ALAA-20001@users.noreply.github.com>
2024-02-29 14:20:38 +0200

    home page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -19,68 +19,79 @@ instagram
 
 
         <div class="relative mt-1">
-    <div class="absolute top-2 left-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
-    </div>
-    <input type="text" placeholder="Search"
-        class="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
-</div>
-
-<div class="flex space-x-4 items-center">
-    <svg xmlns="http://www.w3.org/2000/svg"
-        class="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"
-        viewBox="0 0 20 20" fill="currentColor">
-        <path
-            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg"
-        class="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" fill="none"
-        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-        <path stroke-linecap="round" stroke-linejoin="round"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-    </svg>
-    <!-- ------------------- style drop down------------------------ -->
-    <style>
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-.desc {
-  padding: 15px;
-  text-align: center;
-}
-</style>
-</head>
-<!-- ====================dropdown============== -->
-
-<div class="dropdown">
-<img src="https://icon-library.com/images/person-icon-png/person-icon-png-13.jpg" alt="Cinque Terre" width="25" height="25">
-  <div class="dropdown-content">
- <a href="#"><div class="desc" >view profile</div></a>
- <hr>
- <a href=""><div class="desc">log out</div></a> 
-  </div>
-</div>
-</div>
-
+            <div class="absolute top-2 left-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+            <input type="text" placeholder="Search"
+                class="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
         </div>
+
+        <div class="flex space-x-4 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                class="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"
+                viewBox="0 0 20 20" fill="currentColor">
+                <path
+                    d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                class="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out bi bi-bookmark"
+                viewBox="0 0 16 16">
+                <path
+                    d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                class="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <!-- ------------------- style drop down------------------------ -->
+            <style>
+            .dropdown {
+                position: relative;
+                display: inline-block;
+            }
+
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+                z-index: 1;
+            }
+
+            .dropdown:hover .dropdown-content {
+                display: block;
+            }
+
+            .desc {
+                padding: 15px;
+                text-align: center;
+            }
+            </style>
+            </head>
+            <!-- ====================dropdown============== -->
+
+            <div class="dropdown">
+                <img src="https://icon-library.com/images/person-icon-png/person-icon-png-13.jpg" alt="Cinque Terre"
+                    width="25" height="25">
+                <div class="dropdown-content">
+                    <a href="#">
+                        <div class="desc">view profile</div>
+                    </a>
+                    <hr>
+                    <a href="">
+                        <div class="desc">log out</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
     </div>
 </header>
