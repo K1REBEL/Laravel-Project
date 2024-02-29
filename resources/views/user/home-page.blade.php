ebd4bd977252d9dc8b26a1432527a16fd9d4a64d
ALAA-20001 <151511514+ALAA-20001@users.noreply.github.com>
2024-02-29 20:46:43 +0200

    script

--- resources/views/user/home-page.blade.php
+++ resources/views/user/home-page.blade.php
@@ -51,35 +51,15 @@
                 {{-- <img className="object-cover w-full" src="{{$post->media_urls}}"/> --}}
 
                 <div class="flex justify-between px-4 pt-4">
-                    <div class="flex space-x-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                             class="bi bi-heart" viewBox="0 0 16 16">
-                            <path
-                                d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                             class="bi bi-chat" viewBox="0 0 16 16">
-                            <path
-                                d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                        </svg>
+    <div class="cursor-pointer flex space-x-4">
+        <i class="far fa-heart" style="font-size: 24px;" onclick="toggleIconFill(this)"></i>
+        <i class="far fa-comment" style="font-size: 24px;" onclick="toggleIconFill(this)"></i>
+    </div>
 
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="btn fill=" none
-                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                    </svg>
-                    <span flex justify-between px-4 pt-4>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                         class="bi bi-bookmark" viewBox="0 0 16 16">
-                        <path
-                            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
-                    </svg>
-                </span>
-                </div>
-
-
-
+    <span class="cursor-pointer flex space-x-4">
+        <i class="far fa-bookmark" style="font-size: 24px;" onclick="toggleIconFill(this)"></i>
+    </span>
+</div>
 
                 <!-- {/* Post comments */} -->
                 <p class="p-5 truncate">
@@ -259,9 +239,9 @@
                              src="{{asset('storage/'.$user->profile_photo_path)}}" alt="user-image" />
                     @endif
                     <div class="flex-1 ml-4">
-                        <h2 class="font-mute">{{$user->user_handle}}</h2>
-                        <h3 class="text-sm text-gray-400">Welcome to instagram</h3>
-                    </div>
+                    <h2 class="font-mute">{{$user->user_handle}}</h2>
+                    <h3 class="text-sm text-gray-400">Welcome to instagram</h3> 
+</div>
 
                 </div>
 
@@ -269,4 +249,15 @@
 
     </main>
 
-@stop
+    <script>
+        function toggleIconFill(icon) {
+    if (icon.classList.contains('far')) {
+        icon.classList.remove('far');
+        icon.classList.add('fas');
+    } else {
+        icon.classList.remove('fas');
+        icon.classList.add('far');
+    }}
+    </script>
+
+    @stop
