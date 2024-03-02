3b867658bfe3021dbb35e080087c217c708df298
yousef badr <yousefbadr10@gmail.com>
2024-03-03 00:48:48 +0200

    Edits

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -40,9 +40,7 @@ instagram
             <div class="row">
                 <div class="col-lg-3"></div>
                 <div class="col-lg-3">
-{{--                    <h3 class="text-center">Laravel Autocomplete Search Box</h3><hr>--}}
                     <div class="form-group">
-{{--                        <h4>Type by id, name and email!</h4>--}}
                         <input type="text" name="search" id="search" placeholder="Enter search name" class="form-control" onfocus="this.value=''">
                     </div>
                     <div id="search_list"></div>
