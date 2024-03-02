273d538ec9897d76ce4fad5d95451ad2f099b0bd
yousef badr <yousefbadr10@gmail.com>
2024-03-02 18:00:32 +0200

    Change email

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,13 @@ use Laravel\Fortify\Features;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/welcome', function () {
-    return view('welcomepage.instawelcome');
-});
+ Route::get('/', function () {
+     return view('welcome');
+ });
+
+//Route::get('/', function () {
+//    return view('welcomepage.instawelcome');
+//});
 
 // Route::middleware([
 //     'auth:sanctum',
